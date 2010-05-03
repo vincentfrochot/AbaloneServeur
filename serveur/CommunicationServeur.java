ef37edfa9e9d8fc9973b28e73e602f9f14bbdf0d
Clément <clement@JibMB-3.local>
2010-05-03 22:37:33 +0200

    decode mouvement sur le serveur, méthode toString pour Mouvement

--- serveur/CommunicationServeur.java
+++ serveur/CommunicationServeur.java
@@ -1,7 +1,30 @@
 class CommunicationServeur implements Runnable {
+	public static void main(String[] args) {
+		System.out.println(decodeMouvement((short)531));
+	}
 	
-	public Mouvement decodeMouvement(){
+	/** Code un mouvement en binaire pour l'envoi vers le serveur.
+	*
+	* @param code Le code a decoder en mouvement
+	* @return Le Mouvement 
+	*
+	*  codage : 
+	*     6bits premiere bille
+	*     6bits deuxieme bille
+	*     3bits le vecteur de deplacement
+	*/
+	public static Mouvement decodeMouvement(short code){
+		int premiere;
+		int deuxieme;
+		int vecteur;
 		
+		premiere = (code & 077000) >> 9;
+		deuxieme = (code & 0770) >> 3;
+		vecteur = (code & 07);
+		
+		Mouvement m = new Mouvement((byte)premiere, (byte)deuxieme, (byte)vecteur);
+		
+		return m;
 	}
 	
 	public void run(){
